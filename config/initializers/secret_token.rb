0d254180d19c58dc266090ae797345964f5c3f6a
dgafka <dariss6666@gmail.com>
2014-03-17 20:53:05 +0100

    Rozpoczęcie pracy na projektem z Ruby Rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyGists::Application.config.secret_key_base = '7ff52509e4f1a72f608a5b8f02cb5bad34e89301e23918f8af24f8a0eb158abb1506eb911a05ecdd76b67bd12aee28fc0205ed97911027dde54933abd9c5b7f6'
